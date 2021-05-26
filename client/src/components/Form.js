a24da1817e108f5c498c3b1e40b8022892181847
slify08 <islimaxmc@gmail.com>
2021-05-26 14:27:33 +0800

    added input char limit

--- client/src/components/Form.js
+++ client/src/components/Form.js
@@ -2,38 +2,43 @@ import SendIcon from '@material-ui/icons/Send';
 import HighlightOffIcon from '@material-ui/icons/HighlightOff';
 import CircularProgress from '@material-ui/core/CircularProgress';
 
-function Form({
-  setShowForm,
-  setUsername,
-  setPost,
-  username,
-  post,
-  submit,
-  sharing,
-}) {
+function Form(props) {
+  const maxChar = props.post.length;
+
   return (
-    <form spellCheck="false" onSubmit={submit}>
-      <HighlightOffIcon className="close" onClick={() => setShowForm(false)} />
+    <form spellCheck="false" onSubmit={props.submit}>
+      <HighlightOffIcon
+        className="close"
+        onClick={() => props.setShowForm(false)}
+      />
       <div className="input">
         <label>username</label> <br />
         <input
+          maxLength="20"
           type="text"
           placeholder="leave empty for anonymous"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
+          value={props.username}
+          onChange={(e) => props.setUsername(e.target.value)}
         />
       </div>
 
       <div className="input">
         <label>share mo lang</label> <br />
         <textarea
-          value={post}
-          onChange={(e) => setPost(e.target.value)}
+          maxLength="200"
+          value={props.post}
+          onChange={(e) => props.setPost(e.target.value)}
         ></textarea>
+        <div
+          className="max-char"
+          style={{ color: maxChar == 200 && '#ED1D24' }}
+        >
+          {maxChar}/200
+        </div>
       </div>
 
       <div className="send">
-        {sharing ? (
+        {props.sharing ? (
           <CircularProgress className="submit-loader" />
         ) : (
           <div className="submit">
