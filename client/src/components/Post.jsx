77ded7a6cef6c0b4d667027d2f855a27886ae10d
slify08 <islimaxmc@gmail.com>
2021-06-01 17:14:01 +0800

    fixed bugs

--- client/src/components/Post.jsx
+++ client/src/components/Post.jsx
@@ -1,9 +1,12 @@
+import { motion } from 'framer-motion';
 import DeleteForeverIcon from '@material-ui/icons/DeleteForever';
 
 function Post({ post, setShowDp }) {
   return (
     <div className="post">
       <DeleteForeverIcon
+        as={motion.svg}
+        animate={{ rotate: 360 }}
         style={{ fontSize: 18 }}
         onClick={() => setShowDp(true)}
       />
